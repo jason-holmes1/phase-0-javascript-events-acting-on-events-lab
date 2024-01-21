7e3d178ffd91cd69ce73c07d8162db21acd468f7
Jason <holmesjason481@gmail.com>
2024-01-21 14:52:29 -0500

    Event listener moving

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+function moveDodgerLeft() {
+
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+function moveDodgerRight() {
+
+    if (left < 400) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (event) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
